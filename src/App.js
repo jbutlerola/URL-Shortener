bca97d7fc154a3075b1b07be7ffff95d57da134b
Jack Butler <jackbutlerola@gmail.com>
2022-06-14 11:25:41 -0700

    Added showcase section

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Advanced } from "./components/Advanced";
 import { Boost } from "./components/Boost";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Showcase } from "./components/Showcase";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Showcase />
       <Advanced />
       <Boost />
       <Footer />
